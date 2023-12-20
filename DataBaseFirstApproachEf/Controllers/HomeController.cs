15a95a073be210a4d46e90d977b6b4d48d6e737e
Ankit Prasad <ankit.prasad@interactive12.com>
2023-12-20 10:23:43 +0530

    Add project files.

---
+++ DataBaseFirstApproachEf/Controllers/HomeController.cs
@@ -0,0 +1,97 @@
+﻿using DataBaseFirstApproachEf.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace DataBaseFirstApproachEf.Controllers
+{
+    public class HomeController : Controller
+    {
+        DatabaseFirstEFEntities db = new DatabaseFirstEFEntities(); 
+        // GET: Home
+        public ActionResult Index()
+        {
+            var data = db.students.ToList();
+            return View(data);
+        }
+
+        public ActionResult Details()
+        {
+            var data = db.students.ToList();
+            return View(data);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(student s) 
+        {
+            if (ModelState.IsValid == true)
+            {
+                db.students.Add(s);
+                int a = db.SaveChanges();
+                if (a > 0)
+                {
+                    TempData["InsertMessage"] = "<script>alert('Inserted !!')</script>";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["InsertMessage"] = "<script>alert(' Not Inserted !!')</script>";
+                }
+            }
+
+            return View();
+        }
+        public ActionResult Edit(int id)
+        {
+            var row = db.students.Where(model => model.id == id).FirstOrDefault();
+            return View(row);
+        }
+        [HttpPost]
+        public ActionResult Edit(student s)
+        {
+            if (ModelState.IsValid == true)
+            {
+                db.Entry(s).State = EntityState.Modified;
+                int a = db.SaveChanges();
+                if (a > 0)
+                {
+                    TempData["UpdateMessage"] = "<script>alert('Updated !!')</script>";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["UpdateMessage"] = "<script>alert(' Not Updated !!')</script>";
+                }
+            }
+            return View();
+        }
+        public ActionResult Delete(int id)
+        {
+            var DeletedRow = db.students.Where(model => model.id == id).FirstOrDefault();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Delete(student s)
+        {
+            db.Entry(s).State = EntityState.Deleted;
+            int a = db.SaveChanges();
+            if (a > 0)
+            {
+                TempData["DeleteMessage"] = "<script>alert('DEleted !!')</script>";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["DeleteMessage"] = "<script>alert(' Not Deleted !!')</script>";
+            }
+            return View();
+        }
+    }
+}
---
+++ DataBaseFirstApproachEf/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DataBaseFirstApproachEf.MvcApplication" Language="C#" %>
